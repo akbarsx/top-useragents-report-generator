3717fb550b089cace909f9ddb9daef5ebe15b0dd
Lord_Alfred <lord-alfred@yandex.ru>
2018-10-12 18:18:06 +0300

    rewrite for class-based style (💩 code)

--- report.php
+++ report.php
@@ -16,118 +16,147 @@ use DeviceDetector\DeviceDetector;
 $user_agents_file = __DIR__ . '/data/top-user-agents.txt';
 // useragent lines contains count how much it UA as often met on the logs
 $user_agents_countains_counts = true;
-// md5 of useragents file
-$user_agents_file_md5 = md5_file($user_agents_file);
 
-// results file
-$results_file = __DIR__ . '/results/' . $user_agents_file_md5 . '.html';
-// results file location
-$results_file_loc = str_replace(__DIR__, '', $results_file);
+//
+$rg = new ReportGenerator($user_agents_file, $user_agents_countains_counts);
+$rg->process();
 
 
 
-// if report for current useragents already created - redirect
-if (file_exists($results_file)) {
-    header('Location: ' . $results_file_loc);
-    echo 'Report already generated: <a href="' . $results_file_loc . '">view</a>.';
-    exit();
-}
+class ReportGenerator {
 
+    private $ua_path;
+    private $contains_counts;
 
-function escape_row_data($data) {
-    return str_replace(
-        array(
-            "\r\n",
-            "\r",
-            "\n",
-            '   ',
-            '  ',
-            "'"
-        ),
-        array(
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            "\'"
-        ),
-        $data
-    );
-}
+    private $results_file;
+    private $results_file_loc;
 
+    private $tpl_start;
+    private $tpl_row;
+    private $tpl_end;
 
-$tpl_start = file_get_contents(__DIR__ . '/result_tpl/start.html');
-$tpl_start = str_replace('[UA_FILE]', $user_agents_file, $tpl_start);
+    public function __construct($ua_path, $contains_counts) {
+        $this->ua_path = $ua_path;
+        $this->contains_counts = $contains_counts;
 
-$tpl_row = file_get_contents(__DIR__ . '/result_tpl/row.html');
+        // md5 of useragents file
+        $user_agents_file_md5 = md5_file($this->ua_path);
 
-$tpl_end = file_get_contents(__DIR__ . '/result_tpl/end.html');
+        // results file
+        $this->results_file = __DIR__ . '/results/' . $user_agents_file_md5 . '.html';
+        // results file location
+        $this->results_file_loc = str_replace(__DIR__, '', $this->results_file);
 
-
-$user_agents_data = file($user_agents_file);
-$dd = new DeviceDetector();
-$results_handler = fopen($results_file, 'w');
-fwrite($results_handler, $tpl_start);
-
-
-$line_pos = 1;
-foreach ($user_agents_data as $ua) {
-    $ua = trim($ua);
-    if ($user_agents_countains_counts) {
-        $first_space = mb_strpos($ua, ' ');
-        $ua = mb_substr($ua, $first_space + 1);
+        $this->init_templates();
     }
 
-
-    $dd->setUserAgent($ua);
-    $dd->parse();
-
-    $ua_is_bot = $dd->isBot();
-    $clientInfo = $osInfo = $device = $brand = $model = '-';
-    $isBotClass = 'alert-danger';
-    if (!$ua_is_bot) {
-        $isBotClass = '';
-        $clientInfo = $dd->getClient();
-        $osInfo = $dd->getOs();
-        $device = $dd->getDeviceName();
-        $brand = $dd->getBrandName();
-        $model = $dd->getModel();
+    // if report for current useragents already created - redirect
+    private function check_report() {
+        if (file_exists($this->results_file)) {
+            header('Location: ' . $this->results_file_loc);
+            echo 'Report already generated: <a href="' . $this->results_file_loc . '">view</a>.';
+            exit();
+        }
     }
 
-    $current_row = str_replace(
-        array(
-            '[isBotClass]',
-            '[N]',
-            '[UA]',
-            '[isBot]',
-            '[getClient]',
-            '[getOs]',
-            '[getDeviceName]',
-            '[getBrandName]',
-            '[getModel]'
-        ),
-        array(
-            $isBotClass,
-            $line_pos,
-            escape_row_data(htmlspecialchars($ua)),
-            (($ua_is_bot == 1) ? "True" : 'False'),
-            escape_row_data(var_export($clientInfo, true)),
-            escape_row_data(var_export($osInfo, true)),
-            escape_row_data($device),
-            escape_row_data($brand),
-            escape_row_data($model)
-        ),
-        $tpl_row
-    );
+    private function init_templates() {
+        $this->tpl_start = file_get_contents(__DIR__ . '/result_tpl/start.html');
+        $this->tpl_start = str_replace('[UA_FILE]', $this->ua_path, $this->tpl_start);
 
-    fwrite($results_handler, $current_row);
+        $this->tpl_row = file_get_contents(__DIR__ . '/result_tpl/row.html');
 
-    $line_pos++;
+        $this->tpl_end = file_get_contents(__DIR__ . '/result_tpl/end.html');
+    }
+
+    public function process() {
+        $this->check_report();
+
+        $user_agents_data = file($this->ua_path);
+        $dd = new DeviceDetector();
+        $results_handler = fopen($this->results_file, 'w');
+        fwrite($results_handler, $this->tpl_start);
+
+
+        $line_pos = 1;
+        foreach ($user_agents_data as $ua) {
+            $ua = trim($ua);
+            if ($this->contains_counts) {
+                $first_space = mb_strpos($ua, ' ');
+                $ua = mb_substr($ua, $first_space + 1);
+            }
+
+            $dd->setUserAgent($ua);
+            $dd->parse();
+
+            $ua_is_bot = $dd->isBot();
+            $clientInfo = $osInfo = $device = $brand = $model = '-';
+            $isBotClass = 'alert-danger';
+            if (!$ua_is_bot) {
+                $isBotClass = '';
+                $clientInfo = $dd->getClient();
+                $osInfo = $dd->getOs();
+                $device = $dd->getDeviceName();
+                $brand = $dd->getBrandName();
+                $model = $dd->getModel();
+            }
+
+            $current_row = str_replace(
+                array(
+                    '[isBotClass]',
+                    '[N]',
+                    '[UA]',
+                    '[isBot]',
+                    '[getClient]',
+                    '[getOs]',
+                    '[getDeviceName]',
+                    '[getBrandName]',
+                    '[getModel]'
+                ),
+                array(
+                    $isBotClass,
+                    $line_pos,
+                    $this->escape_row_data(htmlspecialchars($ua)),
+                    (($ua_is_bot == 1) ? "True" : 'False'),
+                    $this->escape_row_data(var_export($clientInfo, true)),
+                    $this->escape_row_data(var_export($osInfo, true)),
+                    $this->escape_row_data($device),
+                    $this->escape_row_data($brand),
+                    $this->escape_row_data($model)
+                ),
+                $this->tpl_row
+            );
+
+            fwrite($results_handler, $current_row);
+
+            $line_pos++;
+        }
+
+        fwrite($results_handler, $this->tpl_end);
+
+        fclose($results_handler);
+
+        echo 'Done! Report generated: <a href="' . $this->results_file_loc . '">view</a>.';
+    }
+
+    private function escape_row_data($data) {
+        return str_replace(
+            array(
+                "\r\n",
+                "\r",
+                "\n",
+                '   ',
+                '  ',
+                "'"
+            ),
+            array(
+                ' ',
+                ' ',
+                ' ',
+                ' ',
+                ' ',
+                "\'"
+            ),
+            $data
+        );
+    }
 }
-
-fwrite($results_handler, $tpl_end);
-
-fclose($results_handler);
-
-echo 'Done! Report generated: <a href="' . $results_file_loc . '">view</a>.';
